d3e47ff8898f92e0a7c88e38133f79a4141e3041
Marsa <mgomezlavall@gmail.com>
2020-10-12 18:52:50 +0200

    export default title

--- src/components/Text/Title.tsx
+++ src/components/Text/Title.tsx
@@ -1,4 +1,4 @@
 import Text from "./Text";
-export const Title = (props) => (
-  <Text {...props} as="h1" size="$7" fontWeight="800" />
-);
+const Title = (props) => <Text {...props} as="h1" size="$7" fontWeight="800" />;
+
+export default Title;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import dynamic from "next/dynamic";
 import React from "react";
 import { Container } from "../components/Container/Container";
 import { MainLayout } from "../components/MainLayout/MainLayout";
-import { Title } from "../components/Text/Title";
+import Title from "../components/Text/Title";
+import Text from "../components/Text/Text";
 
 const Hydra = dynamic(() => import("../containers/Hydra"), { ssr: false });
 
